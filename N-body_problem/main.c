62211169c44c47447e236b233cb8232b4dfa4576
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2015-05-02 12:48:29 +0200

    Initial Commit

---
+++ N-body_problem/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  N-body_problem
+//
+//  Created by Sergii Gryshkevych on 02/05/15.
+//  Copyright (c) 2015 Sergii Gryshkevych. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
